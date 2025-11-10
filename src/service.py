564dbe611533d346c800ca2f38b7cca0fb5eecac
Sema2944 <semenkin4@gmail.com>
2025-11-10 18:39:04 +0300

    Update service.py

--- src/service.py
+++ src/service.py
@@ -1,8 +1,4 @@
 import uvicorn, os
-
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8000"))
-    # теперь указываем на корневой файл service.py
-    uvicorn.run("service:app", host="0.0.0.0", port=port, reload=False)
-
-    return {"picks": picks}
+    port = int(os.getenv("PORT","8000"))
+    uvicorn.run("src.service:app", host="0.0.0.0", port=port, reload=False)
