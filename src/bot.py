75028245ecbf82a5f798d811e5c411e172a392d4
Sema2944 <semenkin4@gmail.com>
2025-11-29 19:19:00 +0300

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -8,7 +8,7 @@ from aiogram.filters import Command
 from api_sport_client import ApiSportClient
 
 # 🔹 1) ВСТАВЬ ТОКЕН ОТ BotFather
-TELEGRAM_BOT_TOKEN = "8258369679:AAHuDXX5GBSBQd181nx45vP-Ubk1t3NHj7I"
+TELEGRAM_BOT_TOKEN = "8258369679:AAGgD5mjO-SbQPoY91K1s-CZ5ELGAWx059g"
 
 # 🔹 2) ВСТАВЬ API-ключ из https://app.api-sport.ru/dashboard
 API_SPORT_KEY = "95169de3-6577-4d35-acf9-395edfc18f98"
