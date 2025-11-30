3c06201dc9f3b032848020ad141ff19b52dc71cd
Sema2944 <semenkin4@gmail.com>
2025-11-30 18:56:30 +0300

    Update run_telegram.py

--- scripts/run_telegram.py
+++ scripts/run_telegram.py
@@ -1,6 +1,6 @@
+# scripts/run_telegram.py
+
 from src.telegram_bot import main
 
 if __name__ == "__main__":
-    # Просто вызываем main() из src/telegram_bot.py
-    # Там уже всё настроено: хендлеры, polling, логирование.
     main()
