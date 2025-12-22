5e9f40045f8d710201e948b563eb6f616e4b6c15
Sema2944 <semenkin4@gmail.com>
2025-12-22 11:32:24 +0300

    Update run_telegram.py

--- scripts/run_telegram.py
+++ scripts/run_telegram.py
@@ -1,5 +1,6 @@
 # scripts/run_telegram.py
+
 import runpy
 
 if __name__ == "__main__":
-    runpy.run_module("src.telegram_bot", run_name="__main__")
+    runpy.run_module("src.telegram_bot.bot", run_name="__main__")
